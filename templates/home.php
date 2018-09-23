736be571f01a825bba64ff3632e01cbb2a779bef
Shiny Alex <carvantes.alex@gmail.com>
2018-09-22 23:29:34 -0500

    Inicial Commit-Full HTML home only

---
+++ templates/home.php
@@ -0,0 +1,241 @@
+<?php
+    /* Template Name:  home*/ 
+//echo "<section id ='shiny-template'>";
+    get_header();
+?>
+
+
+
+
+
+
+
+<section id="primera" class="template-section">
+    <div class="contain">
+         <div class="center">
+             <h2>
+                 Pide hoy tu primera Asesoría
+                y recibe un<br>
+                <div class="blue friend">
+                     10% de Descuento
+                </div> 
+                <a href="" class="btn">
+                    ¡Me interesa!
+                </a>
+             </h2>
+         </div>
+         
+
+       
+       
+       
+    </div>
+</section>
+
+<section id="asesoria">
+    <div class="contain">
+        <div class="center">
+            <h1 class="blue friend">
+                <span class="navy">
+                    Asesoría en 
+                </span>
+                Cambio de Hábitos
+                <span class="normal">
+                    Online
+                </span>
+            </h1>
+            
+            <h2 class="green">¡Esto no es una dieta!</h2>
+            
+            <p>
+              Te ayudo a mejorar tú
+               <span class="blue">
+                BIENESTAR INTEGRAL 
+            </span>
+            para que
+            mejores tus hábitos y disfrutes de la calidad de vida que
+            mereces. Con alimentación viva que proviene de la naturaleza todo a base de alimentos sanos que contribuyen a que tú cuerpo físico viva en armonía total.
+
+            </p>
+            
+            <p>
+                "Los cambios se hacen de manera gradual para que te acompañen a lo largo de tú vida. 
+            </p>
+            
+            <p class="green friend">
+                ¡La salud es un gran tesoro!
+            </p>
+            
+        </div>
+    </div>
+</section>
+
+
+<section id="beneficios" class="template-section">
+    <div class="contain">
+         <div class="flex">
+            
+            <div class="col small">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/kiwi.png" alt="Kiwi" id="kiwi">
+                <h2>BENEFICIOS</h2>
+                <p>¡A tu ritmo y en tu tiempo!</p>
+            </div>
+            <div class="col big">
+                <p>Te ayudaré a cambiar tus hábitos para mejorar la calidad de alimentación que ingresa a tu cuerpo físico obteniendo los siguientes beneficios:</p>
+                <ul>
+                    <li>Asesoria individual en el proceso de cambio, sin juicios, solo  motivación, comprensión y energía positiva para tu éxito en el cambio de hábitos.</li>
+                    <li> Cambio de hábitos sanos que duraran toda tu vida.</li>
+                    <li>Incrementaras energía y vitalidad consumiendo alimentos  vivos</li>
+                    <li>Aprenderás a comprar para nutrir,  seleccionando y combinando que consumes diariamente.</li>
+                    <li>Técnicas de cocinar de manera natural, alimentos riquísimos y económicos.</li>
+                    <li>Harás consciencia de que SOMOS SERES ÚNICOS E IRREPETIBLES.</li>
+                </ul>
+                <p class="yellow">
+                    ¡Y todo esto desde la comodidad de tu casa!
+                </p>
+            </div>
+            
+             
+             
+         </div>
+
+    </div>
+</section>
+
+
+<section id="testimonios" class="template-section">
+    <div class="contain">
+         <div class="center">
+             
+             <h2 class="blue">
+                 Testimonios
+             </h2>
+             
+             <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/test-marine.png" alt="Foto de manny Reyes">
+             <h3 class="blue">
+                 <span class="friend">
+                     Manny Reyes
+                 </span>
+                 <span class="normal">
+                     Usa Marine
+                 </span> 
+             </h3>
+             <p>
+                 La ALIMENTACIÓN NATURAL ha ayudado en mi entrenamiento físico y MENTAL estoy más enfocado lleno de ENERGÍA siempre ATENTO sobretodo con buena disponibilidad. Mis compañeros me preguntan cómo le hago para estar siempre de BUEN HUMOR y con mi ENERGÍA ELEVADA les digo que es GRACIAS a la ALIMENTACIÓN VIVA
+
+             </p>
+             
+       
+             
+             <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/test-marine.png" alt="Foto de manny Reyes">
+             <h3 class="blue">
+                 <span class="friend">
+                     
+                 </span>
+                 <span class="normal">
+                     
+                 </span> 
+             </h3>
+             <p>
+                 
+             </p>
+             
+         </div>
+         
+
+       
+       
+       
+    </div>
+</section>
+
+
+<section id="faq" class="template-section">
+    <div class="contain">
+         <div class="center">
+             
+             
+             
+         </div>
+    </div>
+</section>
+
+
+
+<section id="precio" class="template-section">
+    <div class="contain">
+         <div class="center fcentered">
+             
+             <h2 class="friend">Precio</h2>
+             <p>Por tan sólo</p>
+             <p id="usd" class="green">40 USD</p>
+             
+             <div class="flex mini">
+                 <div class="fcentered">
+                     <div class="col">
+                         <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/paypal.png" alt="PayPal">
+                     </div>
+                     <div class="col">
+                         <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/002-visa.png" alt="VISA">
+                     </div>
+                 </div>
+             </div>
+             
+             <a href="" class="btn">
+                    ¡Comprar!
+            </a>
+         </div>
+         
+         <h2 class="blue padtop">¿Cómo comprar?</h2>
+         <div class="flex cuartil">
+             <div class="col">
+                 <div class="fcentered"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/003-click.png" alt="Click"></div>
+                 <p>1. Haz click en el botón COMPRAR</p>
+             </div>
+             <div class="col">
+                 <div class="fcentered"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/004-payment-method.png" alt="Método de pago"></div>
+                 <p>2. Realiza tu pago</p>
+             </div>
+             <div class="col">
+                 <div class="fcentered"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/005-chat.png" alt="Chat"></div>
+                 <p>3. Contáctame para concertar una cita</p>
+                 <p class="txt-mini">y elegir juntos el canal online para llevarla a cabo.</p>
+             </div>
+             <div class="col">
+                 <div class="fcentered"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/006-salad.png" alt="Ensalada"></div>
+                 <p>4. ¡Y listo!</p>
+                 <p class="txt-mini">comienza ya a recibir todos los beneficios de una nutrición realmente saludable.</p>
+             </div>
+         </div>
+    </div>
+</section>
+
+<section id="aboutme">
+    <div class="contain">
+        <div class="flex">
+            <div class="col empty"></div>
+            <div class="col just-center">
+                <h2 class="blue friend">Acerca de mi</h2>
+                
+                <p>¡HOLA!  Soy Claudia Ruiz, Mexicana con mucho orgullo y me gusta ayudar a las personas a sentirse más saludables por fuera y por dentro.</p>
+                <p>Estudié TROFOLOGÍA ,  COACH EN CAMBIO DE HÁBITOS,  PLANT BASED CHEF,  AYURVEDA , Doshas y muchos tips para mejorar tu calidad de vida. </p>
+                <p>Todo comenzó cuando llego a mi vida una condición de enfermedad llamada Cistitis Intersticial Crónica (según la Ciencia Médica no hay cura) así que decidí investigar mucho y aprender sobre la Alimentación Consciente y la perfecta combinación de los alimentos que ingerimos diariamente para alimentar el Cuerpo, Mente y Espíritu.</p>
+                
+                <a href="" class="btn">
+                    Saber más
+                </a>
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+<?php 
+    //include("shiny-footer.php");
+    get_footer();
+?>
+
---
+++ templates/shiny-footer.php
@@ -0,0 +1,89 @@
+<section id="contacto">
+    <div class="container">
+        <div class="flex">
+            <div class="col c-col">
+                <h2>
+                  ¡Contáctame
+                  
+                </h2>
+                
+                <p class="pad-top">
+                    Coméntanos que servicio te interesa y te pondremos en contacto con el profesional que necesitas:
+                </p>
+                
+                X<div id="mail-cols" class="flex">X
+                    <div class="col icon fcentered">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/mail.png" alt="email">
+                    </div>
+                    <div class="col txt fcentered">
+                        
+                         
+                         
+                        
+                    </div>
+                X</div>X
+                
+                <div id="cform">
+                     <?php echo do_shortcode("[caldera_form id=CF5b766908b0b8a]"); ?>
+                </div>
+                
+                
+                
+            </div>
+            
+            
+            
+            <div class="col" id="mapa">
+
+                <div class="flex icons-bot">
+                    <div class="col icon fcentered ">
+                        <div class="flex">
+                            <img src="../img/home/002-telephone.png" alt="Teléfono">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/whatsapp.png" alt="WhatsApp">
+                        </div>
+                        
+                    </div>
+                    <div class="col txt ">
+                        <p class="white phone">
+                            <a class="white" href="https://wa.me/14158791869">+1 415 8791869</a><br>
+                        </p>
+                    </div>
+                </div>
+               
+                <div class="flex icons-bot">
+                    <div class="col icon fcentered ">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/phone.png" alt="teléfono">
+                    </div>
+                    <div class="col txt">
+                        <p class="white">
+                            De Lunes a Viernes<br> 
+                            de 9:00 am a 5:00 pm 
+                        </p>
+                    </div>
+                </div>
+                
+                <div class="flex icons-bot">
+                    <div class="col icon fcentered">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/clock.png" alt="horario">
+                    </div>
+                    <div class="col txt totop">
+                        <a href="mailto:contacto@bodybalance.mx?subject=Contacto%20Bodybalance"><p class="">claudia@habitosaludables.com</p></a>
+                    </div>
+                </div>
+                
+            </div>
+            
+            
+            
+        </div>
+        
+                       
+    </div>
+    
+    <!--Start of Zendesk Chat Script-->
+
+<!--End of Zendesk Chat Script-->
+
+</section>
+
+</section>
